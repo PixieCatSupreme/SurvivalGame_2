0ef908cbc1eddf622842d1857e9d864c601eee44
Joell Stehouwer <joell@stehouwer.net>
2015-09-02 16:40:17 +0200

    added actor

--- src/SurvivalGame/Server/Server/Creature.cs
+++ src/SurvivalGame/Server/Server/Creature.cs
@@ -3,30 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Mentula.Utilities;
+using Microsoft.Xna.Framework;
 
 namespace Mentula.Server
 {
-    public class Creature
+    public class Creature : Actor
     {
+        
         public string Name;
         public Stats Stats;
         public int[] Health;
         public int[] MaxHealth;
 
         public Creature()
+            : base()
         {
             Name = "The Unnameable";
             Stats = new Stats();
-            Health = new int[6] { 0, 0, 0, 0, 0, 0 };
-            MaxHealth = new int[6] { 0, 0, 0, 0, 0, 0 };
+            Health = new int[6] { dh, dh, dh, dh, dh, dh };
+            MaxHealth = new int[6] { dh, dh, dh, dh, dh, dh };
         }
 
-        public Creature(string n, Stats s, int h)
+        public Creature(string n, Vector2 pos, IntVector2 chunkpos)
+            : base(pos, chunkpos)
+        {
+            Name = n;
+            Stats = new Stats();
+            Health = new int[6] { dh, dh, dh, dh, dh, dh };
+            MaxHealth = new int[6] { dh, dh, dh, dh, dh, dh };
+        }
+
+        public Creature(string n, Stats s, int h, Vector2 pos, IntVector2 chunkpos)
+            : base(pos, chunkpos)
         {
             Name = n;
             Stats = s;
             Health = new int[6] { h, h, h, h, h, h };
             MaxHealth = new int[6] { h, h, h, h, h, h };
         }
+
+        private const int dh = 50;
     }
 }
