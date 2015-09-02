7639d1ecb0a2df0bb2231cc0133177d5553e8324
Joell Stehouwer <joell@stehouwer.net>
2015-09-02 16:02:26 +0200

    Initial Creature

---
+++ src/SurvivalGame/Server/Server/Creature.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mentula.Server
+{
+    public class Creature
+    {
+        public string Name;
+        public Stats Stats;
+        public int[] Health;
+        public int[] MaxHealth;
+
+        public Creature()
+        {
+            Name = "The  Unnameable";
+            Stats = new Stats();
+            Health = new int[6] { 0, 0, 0, 0, 0, 0 };
+            MaxHealth = new int[6] { 0, 0, 0, 0, 0, 0 };
+        }
+
+        public Creature(string n,Stats s, int h)
+        {
+            Name = n;
+            Stats = s;
+            Health = new int[6] { h, h, h, h, h, h };
+            MaxHealth = new int[6] { h, h, h, h, h, h };
+        }
+    }
+}
