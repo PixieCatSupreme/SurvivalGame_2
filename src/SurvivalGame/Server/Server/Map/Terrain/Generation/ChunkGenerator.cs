f258db1353befe18bb768dd9e4b4643fff2351a2
Joell Stehouwer <joell@stehouwer.net>
2015-09-03 16:16:44 +0200

    New terrain generation

--- src/SurvivalGame/Server/Server/Map/Terrain/Generation/ChunkGenerator.cs
+++ src/SurvivalGame/Server/Server/Map/Terrain/Generation/ChunkGenerator.cs
@@ -13,8 +13,8 @@ namespace Mentula.Server
     public static class ChunkGenerator
     {
         private static int cSize = Res.ChunkSize;
-        private static IntVector2 lastPos;
-        private static perlinpoint[] points;
+        private static float[] rainArray;
+
 
         public static Chunk Generate(ref Chunk chunk)
         {
@@ -26,99 +26,39 @@ namespace Mentula.Server
         private static void GenerateTerrain(ref Chunk chunk)
         {
             chunk.Tiles = new Tile[cSize * cSize];
-
-            if (chunk.ChunkPos != lastPos)
-            {
-                generatePerlinPoints(chunk.ChunkPos);
-                lastPos = chunk.ChunkPos;
-            }
-
-            Vector2 pos = chunk.ChunkPos.ToVector2();
-            double chunkR = 0;
+            rainArray = new float[cSize * cSize];
             for (int i = 0; i < cSize * cSize; i++)
             {
-                double rain = 0;
-                double w = 0;
-                for (int j = 0; j < points.Length; j++)
-                {
-                    float d = Vector2.Distance(points[j].pos, pos);
-                    double weight = 1 / Math.Pow(d, d / 1000) * points[j].w;
-                    rain += points[j].val * weight;
-                    w += weight;
-                }
-                rain /= w;
-                chunkR += rain;
-                chunk.Tiles[i] = new Tile(ChT(rain), new IntVector2(i % cSize, i / cSize));
+                int x = i % cSize + chunk.ChunkPos.X * cSize;
+                int y = i / cSize + chunk.ChunkPos.Y * cSize;
+                rainArray[i] =
+                PerlinNoise.Generate(10, cSize / 4, x, y) +
+                PerlinNoise.Generate(30, cSize * 2, x, y) +
+                PerlinNoise.Generate(60, cSize * 16, x, y);
+                chunk.Tiles[i]= new Tile(ChT(rainArray[i]),new IntVector2(i%cSize,i/cSize));
             }
-
-            chunk.Rain = (float)chunkR / (cSize * cSize);
         }
 
         private static int ChT(double rain)
         {
             int n = -1;
-            if (rain < 0.25)
+            if (rain < 25)
             {
                 n = 0;
             }
-            else if (rain < 0.5)
+            else if (rain < 50)
             {
                 n = 1;
             }
-            else if (rain < 0.75)
+            else if (rain < 75)
             {
                 n = 2;
             }
-            else if (rain < 1)
+            else if (rain < 100)
             {
                 n = 3;
             }
             return n;
-        }
-
-        private static void generatePerlinPoints(IntVector2 pos)
-        {
-            int x = pos.X * cSize;
-            int y = pos.Y * cSize;
-            int a0 = cSize * 16;
-            int a1 = cSize * 4;
-            int a2 = cSize;
-
-            points = new perlinpoint[12];
-
-            points[0] = new perlinpoint(new Vector2(MathEX.FloorAtBi(x, a0), MathEX.FloorAtBi(y, a0)), RNG.RFloatFromString(MathEX.FloorAtBi(x, a0), Res.Seed, MathEX.FloorAtBi(y, a0)), 4);
-            points[1] = new perlinpoint(new Vector2(MathEX.FloorAtBi(x, a0), MathEX.CeilAtBi(y, a0)), RNG.RFloatFromString(MathEX.FloorAtBi(x, a0), Res.Seed, MathEX.CeilAtBi(y, a0)), 4);
-            points[2] = new perlinpoint(new Vector2(MathEX.CeilAtBi(x, a0), MathEX.FloorAtBi(y, a0)), RNG.RFloatFromString(MathEX.CeilAtBi(x, a0), Res.Seed, MathEX.FloorAtBi(y, a0)), 4);
-            points[3] = new perlinpoint(new Vector2(MathEX.CeilAtBi(x, a0), MathEX.CeilAtBi(y, a0)), RNG.RFloatFromString(MathEX.CeilAtBi(x, a0), Res.Seed, MathEX.CeilAtBi(y, a0)), 4);
-
-            points[4] = new perlinpoint(new Vector2(MathEX.FloorAtBi(x, a1), MathEX.FloorAtBi(y, a1)), RNG.RFloatFromString(MathEX.FloorAtBi(x, a1), Res.Seed, MathEX.FloorAtBi(y, a1)), 2);
-            points[5] = new perlinpoint(new Vector2(MathEX.FloorAtBi(x, a1), MathEX.CeilAtBi(y, a1)), RNG.RFloatFromString(MathEX.FloorAtBi(x, a1), Res.Seed, MathEX.CeilAtBi(y, a1)), 2);
-            points[6] = new perlinpoint(new Vector2(MathEX.CeilAtBi(x, a1), MathEX.FloorAtBi(y, a1)), RNG.RFloatFromString(MathEX.CeilAtBi(x, a1), Res.Seed, MathEX.FloorAtBi(y, a1)), 2);
-            points[7] = new perlinpoint(new Vector2(MathEX.CeilAtBi(x, a1), MathEX.CeilAtBi(y, a1)), RNG.RFloatFromString(MathEX.CeilAtBi(x, a1), Res.Seed, MathEX.CeilAtBi(y, a1)), 2);
-
-            points[8] = new perlinpoint(new Vector2(MathEX.FloorAtBi(x, a2), MathEX.FloorAtBi(y, a2)), RNG.RFloatFromString(MathEX.FloorAtBi(x, a2), Res.Seed, MathEX.FloorAtBi(y, a2)), 1);
-            points[9] = new perlinpoint(new Vector2(MathEX.FloorAtBi(x, a2), MathEX.CeilAtBi(y, a2)), RNG.RFloatFromString(MathEX.FloorAtBi(x, a2), Res.Seed, MathEX.CeilAtBi(y, a2)), 1);
-            points[10] = new perlinpoint(new Vector2(MathEX.CeilAtBi(x, a2), MathEX.FloorAtBi(y, a2)), RNG.RFloatFromString(MathEX.CeilAtBi(x, a2), Res.Seed, MathEX.FloorAtBi(y, a2)), 1);
-            points[11] = new perlinpoint(new Vector2(MathEX.CeilAtBi(x, a2), MathEX.CeilAtBi(y, a2)), RNG.RFloatFromString(MathEX.CeilAtBi(x, a2), Res.Seed, MathEX.CeilAtBi(y, a2)), 1);
-
-        }
-
-        private class perlinpoint
-        {
-            public Vector2 pos;
-            public float val;
-            public float w;
-
-            public perlinpoint()
-            {
-            }
-
-            public perlinpoint(Vector2 p, float v, float weight)
-            {
-                pos = p;
-                val = v;
-                w = weight;
-            }
         }
     }
 }
---
+++ src/SurvivalGame/Server/Server/Map/Terrain/Generation/PerlinNoise.cs
@@ -0,0 +1,68 @@
+﻿using Mentula.Utilities;
+using System;
+using System.Collections.Generic;
+using Mentula.Utilities.MathExtensions;
+using Mentula.Utilities.Resources;
+
+namespace Mentula.Server
+{
+    public static class PerlinNoise
+    {
+        private static Dictionary<string, float> NoiseDict = new Dictionary<string, float>();
+
+        public static float Generate(float weight, float frequency, float x, float y)
+        {
+            float xLow = (float)Math.Floor(x / frequency) * frequency;
+            float xHigh = (float)Math.Ceiling(x / frequency) * frequency;
+            float yLow = (float)Math.Floor(y / frequency) * frequency;
+            float yHigh = (float)Math.Ceiling(y / frequency) * frequency;
+            float x0y0 = GetNoise(xLow, yLow);
+            float x0y1 = GetNoise(xLow, yHigh);
+            float x1y0 = GetNoise(xHigh, yLow);
+            float x1y1 = GetNoise(xHigh, yHigh);
+            float x0Noise;
+            float x1Noise;
+            if (yLow == yHigh)
+            {
+                x0Noise = x0y0;
+                x1Noise = x1y0;
+            }
+            else
+            {
+                x0Noise = MathEX.Lerp(x0y0, x0y1, MathEX.InvLerp(yLow, yHigh, y));
+                x1Noise = MathEX.Lerp(x1y0, x1y1, MathEX.InvLerp(yLow, yHigh, y));
+            }
+            if (x0Noise == x1Noise)
+            {
+                return x0Noise * weight;
+            }
+            else
+            {
+                return MathEX.Lerp(x0Noise, x1Noise, MathEX.InvLerp(xLow, xHigh, x)) * weight;
+            }
+        }
+
+        private static float GetNoise(float x, float y)
+        {
+            float n;
+            string s = x.ToString() + Res.Seed + y.ToString();
+
+            if (NoiseDict.Count > 4096)
+            {
+                NoiseDict = new Dictionary<string, float>();
+            }
+
+            if (NoiseDict.ContainsKey(s))
+            {
+                NoiseDict.TryGetValue(s, out n);
+                return n;
+            }
+            else
+            {
+                n = RNG.RFloatFromString(s);
+                NoiseDict.Add(s, n);
+                return n;
+            }
+        }
+    }
+}
