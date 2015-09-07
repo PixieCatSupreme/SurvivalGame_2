83223ea6c76002707e9e67a056c22935a9a1280f
Joell Stehouwer <joell@stehouwer.net>
2015-09-07 15:44:19 +0200

    Initial NPC

---
+++ src/SurvivalGame/Server/Server/NPC.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mentula.Utilities;
+using Microsoft.Xna.Framework;
+
+namespace Mentula.Server
+{
+    public class NPC:Creature
+    {
+        private Vector2 targetArea;
+        private double lastAttackTime;
+
+        public void dostuff(float deltaTime,Creature[] players)
+        {
+            GetTarget();
+        }
+
+        private bool GetTarget()
+        {
+            bool t = false;
+
+            return t;
+        }
+
+    }
+}
