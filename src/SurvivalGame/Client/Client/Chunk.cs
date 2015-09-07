1b497f82a9f598ca9f76c4e9f8ffee0ea7860389
Frank <largewowfan@gmail.com>
2015-09-07 21:36:46 +0200

    Split chunk

--- src/SurvivalGame/Client/Client/Chunk.cs
+++ src/SurvivalGame/Client/Client/Chunk.cs
@@ -1,14 +1,19 @@
-﻿using Microsoft.Xna.Framework;
+﻿using Mentula.Utilities;
+using Mentula.Utilities.Resources;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.CompilerServices;
-using Resc = Mentula.Utilities.Resources.Res;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Mentula.Utilities
+namespace Mentula.Client
 {
     public class Chunk
     {
         public Tile[] Tiles;
         public IntVector2 ChunkPos;
-        public float Rain;
 
         public Chunk(IntVector2 pos)
         {
@@ -18,32 +23,32 @@ namespace Mentula.Utilities
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Vector2 GetTotalPos(IntVector2 chunk, Vector2 tile)
         {
-            return new Vector2(tile.X + chunk.X * Resc.ChunkSize, tile.Y + chunk.Y * Resc.ChunkSize);
+            return new Vector2(tile.X + chunk.X * Res.ChunkSize, tile.Y + chunk.Y * Res.ChunkSize);
         }
 
         public static unsafe void FormatPos(IntVector2* chunk, Vector2* tile)
         {
-            while(-tile->X < 0 || -tile->Y < 0 || -tile->X > Resc.ChunkSize || -tile->Y > Resc.ChunkSize)
+            while (-tile->X < 0 || -tile->Y < 0 || -tile->X > Res.ChunkSize || -tile->Y > Res.ChunkSize)
             {
                 if (-tile->X < 0)
                 {
-                    tile->X -= Resc.ChunkSize;
+                    tile->X -= Res.ChunkSize;
                     chunk->X++;
                 }
-                else if (-tile->X > Resc.ChunkSize)
+                else if (-tile->X > Res.ChunkSize)
                 {
-                    tile->X += Resc.ChunkSize;
+                    tile->X += Res.ChunkSize;
                     chunk->X--;
                 }
 
                 if (-tile->Y < 0)
                 {
-                    tile->Y -= Resc.ChunkSize;
+                    tile->Y -= Res.ChunkSize;
                     chunk->Y++;
                 }
-                else if (-tile->Y > Resc.ChunkSize)
+                else if (-tile->Y > Res.ChunkSize)
                 {
-                    tile->Y += Resc.ChunkSize;
+                    tile->Y += Res.ChunkSize;
                     chunk->Y--;
                 }
             }
---
+++ src/SurvivalGame/Server/Server/Map/Terrain/Generation/Chunk.cs
@@ -0,0 +1,19 @@
+﻿using Mentula.Utilities;
+using Microsoft.Xna.Framework;
+using System.Runtime.CompilerServices;
+using Resc = Mentula.Utilities.Resources.Res;
+
+namespace Mentula.Server
+{
+    public class Chunk
+    {
+        public Tile[] Tiles;
+        public IntVector2 ChunkPos;
+        public float Rain;
+
+        public Chunk(IntVector2 pos)
+        {
+            ChunkPos = pos;
+        }
+    }
+}
