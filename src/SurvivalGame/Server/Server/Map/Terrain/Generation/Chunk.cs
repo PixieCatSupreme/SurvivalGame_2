a68fcba6c45bcd303177605d16f834048cb5b4bd
Joell Stehouwer <joell@stehouwer.net>
2015-09-13 23:04:51 +0200

    Added destructibles

--- src/SurvivalGame/Server/Server/Map/Terrain/Generation/Chunk.cs
+++ src/SurvivalGame/Server/Server/Map/Terrain/Generation/Chunk.cs
@@ -9,11 +9,13 @@ namespace Mentula.Server
     public class Chunk
     {
         public Tile[] Tiles;
+        public List<Destructible> Destructibles;
         public IntVector2 ChunkPos;
         public float Rain;
 
         public Chunk(IntVector2 pos)
         {
+            Destructibles = new List<Destructible>();
             ChunkPos = pos;
         }
     }
---
+++ src/SurvivalGame/Server/Server/Map/Terrain/Generation/Destructible.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+
+namespace Mentula.Utilities
+{
+    public class Destructible : Actor
+    {
+        public int Id;
+        public int Health;
+        public int MaxHealth;
+
+        public Destructible(IntVector2 chunkPos,Vector2 pos,int id)
+            :base(pos,chunkPos)
+        {
+            Id = id;
+            Health = 100;
+            MaxHealth = 100;
+        }
+
+
+    }
+}
