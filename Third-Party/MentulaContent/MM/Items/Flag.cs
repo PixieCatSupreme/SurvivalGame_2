8b25660cfc20cda9c368fbbe18b5a21f7b067081
Nico <drawolf999@gmail.com>
2015-09-16 10:21:39 +0200

    Items and stuff
    Oh bby

---
+++ Third-Party/MentulaContent/MM/Items/Flag.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Mentula.Content
+{
+    public class Flag
+    {
+        public int ID;
+    }
+}
---
+++ Third-Party/MentulaContent/MM/Items/Item.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Mentula.Content
+{
+    public class Item
+    {
+        public KeyValuePair<Type, object>[] materials;
+        public Item[] itemMaterials;
+        public Flag[] flags;
+        public string name;
+
+        internal Item(KeyValuePair<Type, object>[] materials, Item[] itemMaterials, Flag[] flags, string name)
+        {
+            this.materials = materials;
+            this.itemMaterials = itemMaterials;
+            this.flags = flags;
+            this.name = name;
+        }
+    }
+}
