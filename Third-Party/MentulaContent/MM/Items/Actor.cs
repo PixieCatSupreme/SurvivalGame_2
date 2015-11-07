3e8e5c6f25ddd7cb451ab07bec1767e11a3f6da9
Frank <largewowfan@gmail.com>
2015-11-07 15:32:16 +0100

    Removed usings

--- Third-Party/MentulaContent/MM/Items/Actor.cs
+++ Third-Party/MentulaContent/MM/Items/Actor.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Mentula.Utilities;
-using Microsoft.Xna.Framework;
+﻿using Mentula.Utilities;
 using Mentula.Utilities.Resources;
+using Microsoft.Xna.Framework;
 
 namespace Mentula.Content
 {
