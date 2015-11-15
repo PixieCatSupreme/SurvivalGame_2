3e5fee0c24b7a98e2878277d49215d8df6abf385
Frank <largewowfan@gmail.com>
2015-11-15 13:40:13 +0100

    Item

---
+++ Third-Party/MentulaContent/MM/Items/Item.cs
@@ -0,0 +1,20 @@
+﻿using Mentula.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mentula.Content.MM.Items
+{
+    public class Item
+    {
+        public ulong Id;
+        public uint Volume;
+        public ushort Durability;
+
+        public Pair<string, short>[] Tags;
+        public Item[] Parts;
+        public Material[] Materials;
+    }
+}
---
+++ src/SurvivalGame/Utilities/Pair.cs
@@ -0,0 +1,25 @@
+﻿namespace Mentula.Utilities
+{
+    public struct Pair<Tkey, TValue>
+    {
+        public Tkey Key;
+        public TValue Value;
+
+        public Pair(Tkey key)
+        {
+            Key = key;
+            Value = default(TValue);
+        }
+
+        public Pair(Tkey key, TValue value)
+        {
+            Key = key;
+            Value = value;
+        }
+
+        public override string ToString() 
+        {
+            return Key.ToString() + ": " + Value.ToString();
+        }
+    }
+}
