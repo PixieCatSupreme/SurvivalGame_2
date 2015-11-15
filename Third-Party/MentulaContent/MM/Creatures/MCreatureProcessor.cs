34d0476e947b0564189088547315ff4a624594c1
Frank <largewowfan@gmail.com>
2015-11-15 13:06:46 +0100

    Removed usings

--- Third-Party/MentulaContent/MM/Creatures/MCreatureProcessor.cs
+++ Third-Party/MentulaContent/MM/Creatures/MCreatureProcessor.cs
@@ -1,11 +1,6 @@
 ﻿using Mentula.Utilities;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content.Pipeline;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Mentula.Content.MM.Creatures
 {
