5c540f244bef2632b79371a4ab6b2147bb1c5a36
Frank <largewowfan@gmail.com>
2016-02-07 16:28:28 +0100

    Fixed shift

---
+++ src/SurvivalGame/Client/Client/Menus/GuiMenu.cs
@@ -0,0 +1,18 @@
+﻿using Mentula.GuiItems.Containers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mentula.Client.Menus
+{
+    public sealed class GuiMenu : Menu
+    {
+        private MainGame MGame { get { return (MainGame)Game; } }
+
+        public GuiMenu(MainGame game)
+            : base(game)
+        { }
+    }
+}
