4b219f59c5c4881708e397f4d541dd1166a94abb
Joell Stehouwer <joell@stehouwer.net>
2016-02-08 23:57:15 +0100

    Finnished NPC Path following

--- src/SurvivalGame/Server/Server/NPC.cs
+++ src/SurvivalGame/Server/Server/NPC.cs
@@ -1,4 +1,5 @@
-﻿using Mentula.Utilities;
+﻿//#define ZILTOID
+using Mentula.Utilities;
 using Microsoft.Xna.Framework;
 using Resc = Mentula.Utilities.Resources.Res;
 using Mentula.Utilities.MathExtensions;
@@ -13,13 +14,19 @@ namespace Mentula.Server
     public class NPC : Creature
     {
         private IntVector2[] path;
+        private int moveI;
 
         public NPC(Creature c)
             : base(c)
         {
-            path = new IntVector2[2];
+            moveI = 0;
+#if ZILTOID
+            path = new IntVector2[4];
             path[0] = new IntVector2(-10, -10);
-            path[1] = new IntVector2(10, 10);
+            path[1] = new IntVector2(0, -10);
+            path[2] = new IntVector2(-10, 0);
+            path[3] = new IntVector2(0, 0);
+#endif
         }
 
         public void WalkPath(float deltaTime)
@@ -27,26 +34,24 @@ namespace Mentula.Server
             if (path != null)
             {
                 float speed = deltaTime;
-                Vector2 pos = new Vector2(Pos.X + ChunkPos.X * ChunkSize, Pos.Y + ChunkPos.Y * ChunkSize);
-
-                float dist = Vector2.Distance(pos, path[0]);
-                Vector2 rot = new Vector2(path[0].X-pos.X, path[0].Y - pos.Y);
-                int moveIndex = 0;
-                for (int i = 0; i < path.Length; i++)
+                while (speed > 0 && moveI < path.Length)
                 {
-
+                    Vector2 pos = new Vector2(Pos.X + ChunkPos.X * ChunkSize, Pos.Y + ChunkPos.Y * ChunkSize);
+                    float dist = Vector2.Distance(pos, path[moveI]);
+                    Vector2 rot = new Vector2(path[moveI].X - pos.X, path[moveI].Y - pos.Y);
+                    Rotation = MathEX.VectorToRadians(rot);
+                    if (dist < speed)
+                    {
+                        Pos += rot;
+                        speed -= dist;
+                        moveI++;
+                    }
+                    else
+                    {
+                        Pos += (rot / dist * speed);
+                        speed = 0;
+                    }
                 }
-                if (dist < speed)
-                {
-                    Pos += rot;
-                    speed -= dist;
-                }
-                else
-                {
-                    Pos += (rot / dist * speed);
-                    speed = 0;
-                }
-
                 FormatPos();
             }
         }
