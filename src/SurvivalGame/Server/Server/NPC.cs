904046330b762db35c17e73cbfa042d1fa658a7a
Joell Stehouwer <joell@stehouwer.net>
2016-02-09 15:54:09 +0100

    Test

--- src/SurvivalGame/Server/Server/NPC.cs
+++ src/SurvivalGame/Server/Server/NPC.cs
@@ -33,7 +33,7 @@ namespace Mentula.Server
         {
             if (path != null)
             {
-                float speed = deltaTime*10;
+                float speed = deltaTime * 10;
                 while (speed > 0 && moveI < path.Length)
                 {
                     Vector2 pos = new Vector2(Pos.X + ChunkPos.X * ChunkSize, Pos.Y + ChunkPos.Y * ChunkSize);
@@ -56,6 +56,39 @@ namespace Mentula.Server
             }
         }
 
-        
+        public void GeneratePath(Creature target, List<Chunk> chunks)
+        {
+            Vector2 pos = new Vector2(Pos.X + ChunkPos.X * ChunkSize, Pos.Y + ChunkPos.Y * ChunkSize);
+            Vector2 targetPos = new Vector2(target.Pos.X + target.ChunkPos.X * ChunkSize, target.Pos.Y + target.ChunkPos.Y * ChunkSize);
+            bool inRange = true;
+            bool diff = true;
+            if (Vector2.Distance(pos, targetPos) > 64)
+            {
+                inRange = false;
+            }
+            if (path != null)
+            {
+                if (Vector2.Distance(path[path.Length - 1], targetPos) < 1)
+                {
+                    diff = false;
+                }
+            }
+            if (inRange && diff)
+            {
+                int minX = Math.Min(ChunkPos.X, target.ChunkPos.X) - 1;
+                int maxX = Math.Max(ChunkPos.X, target.ChunkPos.X) + 1;
+                int minY = Math.Min(ChunkPos.Y, target.ChunkPos.Y) - 1;
+                int maxY = Math.Max(ChunkPos.Y, target.ChunkPos.Y) + 1;
+                List<Chunk> c = new List<Chunk>();
+                for (int i = 0; i < chunks.Count; i++)
+                {
+                    if (chunks[i].ChunkPos.X>=minX&& chunks[i].ChunkPos.X <= maxX&& chunks[i].ChunkPos.Y >= minY && chunks[i].ChunkPos.Y <= maxY)
+                    {
+                        c.Add(chunks[i]);
+                    }
+                }
+            }
+        }
+
     }
 }
